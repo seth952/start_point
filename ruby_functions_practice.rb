a6f0c78262b659583872963b1ac6bdf790860ed6
Stephen Campbell <stephencampbe11@hotmail.co.uk>
2019-11-05 18:03:30 +0000

    initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,59 @@
+def return_10()
+  return 10
+end
+
+def add(first_number, second_number)
+  return (first_number + second_number)
+end
+
+def subtract(first_number, second_number)
+
+return subtract(first_number - second_number)
+
+end
+
+def multiply(first_number, second_number)
+  return (first_number * second_number)
+
+end
+def divide(first_number, second_number)
+
+  return (first_number / second_number)
+
+end
+
+def length_of_string()
+  return
+
+
+end
+def join_string()
+
+
+end
+def add_string_as_number()
+
+
+end
+def number_to_full_name__month_1()
+
+
+end
+def number_to_full_name__month_3()
+
+
+end
+
+def number_to_full_name__month_9()
+
+
+end
+def number_to_short_month_name__month_1()
+
+end
+def number_to_short_month_name__month_4()
+
+end
+def number_to_short_month_name__month_10()
+
+end
