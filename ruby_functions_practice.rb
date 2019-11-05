0ab15952a3d93a2b7d406acdcdae593fa25b67c5
Stephen Campbell <stephencampbe11@hotmail.co.uk>
2019-11-05 19:20:26 +0000

    Code added to practice.rb

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -23,37 +23,57 @@ def divide(first_number, second_number)
 end
 
 def length_of_string()
-  return
+  return("A string of length 21".length)
 
 
-end
-def join_string()
+  end
+def join_string(string_1, string_2)
+  return(string_1 + string_2)
+
+  end
 
 
-end
+
 def add_string_as_number()
-
+return("1".to_i + "2".to_i)
 
 end
-def number_to_full_name__month_1()
-
-
+def number_to_full_name__month_3(
+month = 1
+case month
+  when  == 1
+  return "January"
+end
 end
 def number_to_full_name__month_3()
-
-
+  month = 3
+  case month
+    when  == 1
+    return "March"
+end
 end
 
 def number_to_full_name__month_9()
-
-
+  month = 9
+  case month
+    when  == 1
+    return "September"
 end
 def number_to_short_month_name__month_1()
-
+  case month
+    when  == 1
+    return "Jan"
+  end
 end
 def number_to_short_month_name__month_4()
-
+  case month
+  when  == 4
+    return "Apr"
+  end
 end
 def number_to_short_month_name__month_10()
-
+  case month
+  when  == 10
+    return "Oct"
+  end
 end
